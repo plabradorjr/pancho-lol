31d0dea4aa318e2f4c776c47927389561c04d8a3
Pancho Labrador <plabradorjr@gmail.com>
2021-06-23 08:48:27 -0500

    first init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <Container>
+      <Row>
+        <div className="col h1 text-center">test</div>
+      </Row>
+    </Container>
+  )
+}
+
+export default App
